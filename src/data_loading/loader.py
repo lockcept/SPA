ca6a734f7f7a63177ed9eb847b72e49807bf0bcc
lockcept <lockcept@gmail.com>
2024-09-25 18:15:13 +0900

    loader.py

--- src/data_loading/loader.py
+++ src/data_loading/loader.py
@@ -1,27 +1,54 @@
 import os
 import gym
-import d4rl #import 해야 gym.make()에서 d4rl 환경을 불러올 수 있음
+import d4rl  # import 해야 gym.make()에서 d4rl 환경을 불러올 수 있음
 import numpy as np
+import argparse
+
 
 def save_dataset(env_name, dataset):
     save_dir = f"dataset/d4rl/{env_name}"
     os.makedirs(save_dir, exist_ok=True)
-    print (dataset)
-    
-    file_path = os.path.join(save_dir, "dataset.npz")
-    
-    np.savez(file_path, observations=dataset["observations"], actions=dataset["actions"], rewards=dataset["rewards"], terminals=dataset["terminals"], timeouts=dataset["timeouts"]) 
-    print(f"Dataset for {env_name} saved at {file_path}")
 
-def load_antmaze():
-    env_name = "antmaze-umaze-v0"
+    file_path = os.path.join(save_dir, "dataset.npz")
+
+    save_data = {
+        "observations": dataset["observations"],
+        "actions": dataset["actions"],
+        "rewards": dataset["rewards"],
+        "terminals": dataset["terminals"],
+        "timeouts": dataset["timeouts"],
+    }
+
+    for key in dataset.keys():
+        if key.startswith("infos"):
+            save_data[key] = dataset[key]
+
+    np.savez(file_path, **save_data)
+
+    print(f"Dataset saved with keys: {save_data.keys()}")
+
+
+def load(env_name):
     antmaze_env = gym.make(env_name)
     antmaze_dataset = antmaze_env.get_dataset()
-    
+
     save_dataset(env_name, antmaze_dataset)
-    
+
     return antmaze_dataset
 
 
 if __name__ == "__main__":
-    antmaze_dataset = load_antmaze()
+    parser = argparse.ArgumentParser(
+        description="Load and save D4RL dataset for a given environment."
+    )
+
+    parser.add_argument(
+        "--env_name",
+        type=str,
+        default="maze2d-medium-dense-v1",
+        help="Name of the environment to load the dataset for",
+    )
+
+    args = parser.parse_args()
+
+    load(args.env_name)
