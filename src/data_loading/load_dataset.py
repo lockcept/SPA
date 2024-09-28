00390aa141b9c21e98a952d1c0378a2e042c1b88
lockcept <lockcept@gmail.com>
2024-09-28 18:29:07 +0900

    rename files

--- src/data_loading/load_dataset.py
+++ src/data_loading/load_dataset.py
@@ -11,13 +11,27 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"
 from config import DEFAULT_ENV_NAME
 
 
-def load_pair(env_name, pair_name="full_preference_pairs.npz"):
+def load_d4rl_dataset(env_name):
     dir_path = f"dataset/{env_name}"
     dataset_name = "d4rl_dataset.npz"
     dataset = np.load(os.path.join(dir_path, dataset_name))
+
+    return dataset
+
+
+def load_pair(env_name, pair_name="full_preference_pairs.npz"):
+    dir_path = f"dataset/{env_name}"
     pair = np.load(os.path.join(dir_path, pair_name), allow_pickle=True)
 
     return pair
+
+
+def load_processed_data(env_name, pair_name="full_preference_pairs.npz"):
+    dataset = load_d4rl_dataset(env_name)
+    pair = load_pair(env_name, pair_name)
+
+    # todo: check if the dataset and pair are compatible
+    return dataset, pair
 
 
 if __name__ == "__main__":
