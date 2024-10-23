48bf229a1a6383ca60970cd4c6dc005c1fc042ca
lockcept <lockcept@gmail.com>
2024-10-23 15:24:01 +0900

    do not load d4rl if exist

--- src/data_loading/load_d4rl.py
+++ src/data_loading/load_d4rl.py
@@ -5,13 +5,18 @@ import numpy as np
 
 
 def save_d4rl_dataset(env_name):
-    env = gym.make(env_name)
-    dataset = env.get_dataset()
-
     save_dir = f"dataset/{env_name}"
     os.makedirs(save_dir, exist_ok=True)
 
     file_path = os.path.join(save_dir, "d4rl.npz")
+
+    is_already_exist = os.path.exists(file_path)
+    if is_already_exist:
+        print(f"Dataset already exists at {file_path}")
+        return
+
+    env = gym.make(env_name)
+    dataset = env.get_dataset()
 
     save_data = {
         "observations": dataset["observations"],
