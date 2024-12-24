b3981a869790293d9011adea347d51652266a180
lockcept <lockcept@gmail.com>
2024-12-24 19:55:14 +0900

    plot success rate with std

--- src/helper/plot_policy_model.py
+++ src/helper/plot_policy_model.py
@@ -42,22 +42,33 @@ def process_csv_files(csv_files):
         .reset_index()
         .rename(columns={"Reward": "Reward_std"})
     )
-    success_rate_df = (
+    success_mean_df = (
         grouped["Success"]
         .apply(remove_max_min)
         .groupby("Timesteps")
         .apply(lambda x: (x > 0).mean())
         .reset_index()
-        .rename(columns={"Success": "Success_rate"})
+        .rename(columns={"Success": "Success_mean"})
+    )
+    success_std_df = (
+        grouped["Success"]
+        .apply(remove_max_min)
+        .groupby("Timesteps")
+        .apply(lambda x: (((x**2).mean() - x.mean() ** 2) ** 0.5) / ((len(x)) ** 0.5))
+        .reset_index()
+        .rename(columns={"Success": "Success_std"})
     )
 
-    final_df = mean_df.merge(std_df, on="Timesteps").merge(
-        success_rate_df, on="Timesteps"
+    final_df = (
+        mean_df.merge(std_df, on="Timesteps")
+        .merge(success_mean_df, on="Timesteps")
+        .merge(success_std_df, on="Timesteps")
     )
 
     final_df["Reward_mean"] = final_df["Reward_mean"].ewm(alpha=0.2).mean()
     final_df["Reward_std"] = final_df["Reward_std"].ewm(alpha=0.2).mean()
-    final_df["Success_rate"] = final_df["Success_rate"].ewm(alpha=0.2).mean()
+    final_df["Success_mean"] = final_df["Success_mean"].ewm(alpha=0.2).mean()
+    final_df["Success_std"] = final_df["Success_std"].ewm(alpha=0.2).mean()
 
     return final_df
 
@@ -88,7 +99,16 @@ def plot_and_save(df_list, output_path):
     # Success
     plt.subplot(2, 1, 2)
     for mu_algo_name, df in df_list:
-        plt.plot(df["Timesteps"].values, df["Success_rate"].values, label=mu_algo_name)
+        mean_values = df["Success_mean"].values
+        std_values = df["Success_std"].values
+
+        plt.plot(df["Timesteps"].values, mean_values, label=mu_algo_name)
+        plt.fill_between(
+            df["Timesteps"],
+            mean_values - std_values,
+            mean_values + std_values,
+            alpha=0.2,
+        )
     plt.xlabel("Timestep")
     plt.ylabel("Success rate")
     plt.title(f"{output_name} - Success")
@@ -136,7 +156,7 @@ def plot_policy_models(exp_name):
     """
     plot the policy models from hard-coded lists
     """
-    env_list = ["box-close-v2"]
+    env_list = ["button-press-topdown-v2", "box-close-v2", "dial-turn-v2", "sweep-v2"]
     exp_list = [
         f"{exp_name}-00",
         f"{exp_name}-01",
@@ -148,23 +168,8 @@ def plot_policy_models(exp_name):
         "full-binary_MR",
         "full-binary_MR-linear",
         "full-linear_MR-linear",
-        "rnn-full-binary_MR-linear",
-        "rnn-cut-0.5_MR-linear",
-        "rnn-cut-0.25_MR-linear",
-        "rnn-cut-half-random_MR-linear",
-        "rnn-cut-random_MR-linear",
-        "list-2_MR-linear",
-        "list-3_MR-linear",
-        "list-5_MR-linear",
-        "list-11_MR-linear",
-        "rnn-lire-with-0.5_MR",
-        "rnn-lire-with-0.5_MR-linear",
-        "rnn-lire-without-0.5_MR",
-        "rnn-lire-without-0.5_MR-linear",
-        "lire-with-0.5_MR",
-        "lire-with-0.5_MR-linear",
-        "lire-without-0.5_MR",
-        "lire-without-0.5_MR-linear",
+        "rnn-cut-0.75_MR-linear",
+        "rnn-cut-1.0_MR-linear",
     ]
 
     for env_name in env_list:
