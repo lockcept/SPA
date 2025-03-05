8d1419c70eb0ebddc6d643c7f722630211296973
lockcept <lockcept@gmail.com>
2025-03-05 17:14:00 +0900

    fix active margin method

--- src/data_generation/active/margin.py
+++ src/data_generation/active/margin.py
@@ -4,15 +4,40 @@ from typing import List
 
 import numpy as np
 import torch
-from data_generation.raw_pairs import save_raw_pairs
 from data_generation.utils import generate_pairs_from_indices, save_feedbacks_npz
 from reward_learning import get_reward_model, train_reward_model, RewardModelBase
 from utils.path import get_reward_model_path
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+TRAJECTORY_LENGTH = 25
 
 
-def fill_feedback_from_pairs(dataset, pairs, models: List[RewardModelBase]):
+def fill_feedback_from_raw_dataset(cumulative_rewards, pairs):
+    """
+    Fill feedback in dataset using cumulative rewards and calculate mu values.
+    """
+
+    feedbacks = []
+
+    for s0, s1 in pairs:
+        sum_of_rewards_0 = cumulative_rewards[s0[1] - 1] - (
+            cumulative_rewards[s0[0] - 1] if s0[0] > 0 else 0
+        )
+        sum_of_rewards_1 = cumulative_rewards[s1[1] - 1] - (
+            cumulative_rewards[s1[0] - 1] if s1[0] > 0 else 0
+        )
+
+        if np.abs(sum_of_rewards_0 - sum_of_rewards_1) < TRAJECTORY_LENGTH / 2:
+            mu = 0.5
+        else:
+            mu = 0 if sum_of_rewards_0 > sum_of_rewards_1 else 1
+
+        feedbacks.append((s0, s1, mu))
+
+    return feedbacks
+
+
+def fill_feedback_from_models(dataset, pairs, models: List[RewardModelBase]):
     """
     Fill feedback in dataset using multiple reward models and average their mu values.
     Also return the standard deviation of mu values.
@@ -69,7 +94,8 @@ def fill_feedback_from_pairs(dataset, pairs, models: List[RewardModelBase]):
         sum_of_rewards_1 = cumulative_rewards[e1 - 1] - (
             cumulative_rewards[s1 - 1] if s1 > 0 else 0
         )
-        mu = 0 if sum_of_rewards_0 > sum_of_rewards_1 else 1
+
+        mu = sum_of_rewards_1 / (sum_of_rewards_0 + sum_of_rewards_1)
 
         feedbacks.append(((s0, e0), (s1, e1), mu))
 
@@ -89,11 +115,16 @@ def generate_active_margin_pairs(
     reward_model_algo="MR-linear",
     reward_model_count=3,
 ):
-    pairs = []
+    """
+    Generate active margin pairs.
+    """
+    feedbacks = []
     pairs_count_per_round = total_pairs_count // active_round
     pair_algo = "active-margin"
     intermediate_pair_algo = f"{pair_algo}-intermediate"
     obs_dim, act_dim = dataset["observations"].shape[1], dataset["actions"].shape[1]
+
+    cumulative_rewards = np.cumsum(dataset["rewards"], dtype=np.float64)
 
     # remove models if exists
     model_path_pattern = get_reward_model_path(
@@ -110,13 +141,13 @@ def generate_active_margin_pairs(
             print("Removing", model_file)
             os.remove(model_file)
 
-    for _ in range(active_round):
-        if not pairs:
-            pairs = generate_pairs_from_indices(
+    for round_num in range(active_round):
+        if not feedbacks:
+            new_pairs = generate_pairs_from_indices(
                 dataset=dataset,
                 trajectories=traj_set,
                 pair_count=pairs_count_per_round,
-                trajectory_length=25,
+                trajectory_length=TRAJECTORY_LENGTH,
             )
         else:
             for i in range(reward_model_count):
@@ -161,11 +192,11 @@ def generate_active_margin_pairs(
                 dataset=dataset,
                 trajectories=traj_set,
                 pair_count=pairs_count_per_round * pairs_scale,
-                trajectory_length=25,
+                trajectory_length=TRAJECTORY_LENGTH,
             )
 
             # calculate mu values
-            candidate_feedbacks = fill_feedback_from_pairs(
+            candidate_feedbacks = fill_feedback_from_models(
                 dataset=dataset,
                 pairs=candidate_pairs,
                 models=models,
@@ -179,28 +210,30 @@ def generate_active_margin_pairs(
 
             new_feedbacks = sorted_feedbacks[: (total_pairs_count // active_round)]
             new_pairs = [(f[0], f[1]) for f in new_feedbacks]
-            pairs.extend(new_pairs)
+
+            feedbacks.extend(
+                fill_feedback_from_raw_dataset(
+                    cumulative_rewards=cumulative_rewards,
+                    pairs=new_pairs,
+                )
+            )
 
         # save intermediate pairs
-        save_raw_pairs(
+        save_feedbacks_npz(
             env_name=env_name,
             exp_name=exp_name,
             pair_type="train",
-            pairs=pairs,
-            raw_name=intermediate_pair_algo,
+            pair_name=intermediate_pair_algo,
+            feedbacks=feedbacks,
         )
 
-    # save full-binary pairs from selected pairs
-    feedbacks = []
-
-    for i0, i1 in pairs:
-        s0, e0 = i0
-        s1, e1 = i1
-
-        sum_of_rewards_0 = np.sum(dataset["rewards"][s0:e0])
-        sum_of_rewards_1 = np.sum(dataset["rewards"][s1:e1])
-        mu = 0 if sum_of_rewards_0 > sum_of_rewards_1 else 1
-        feedbacks.append(((s0, e0), (s1, e1), mu))
+        save_feedbacks_npz(
+            env_name=env_name,
+            exp_name=exp_name,
+            pair_type="train",
+            pair_name=f"{pair_algo}-round-{round_num}",
+            feedbacks=feedbacks,
+        )
 
     save_feedbacks_npz(
         env_name=env_name,
@@ -210,10 +243,9 @@ def generate_active_margin_pairs(
         feedbacks=feedbacks,
     )
 
-    val_feedbacks = fill_feedback_from_pairs(
-        dataset=dataset,
+    val_feedbacks = fill_feedback_from_raw_dataset(
+        cumulative_rewards=cumulative_rewards,
         pairs=val_pairs,
-        models=models,
     )
 
     save_feedbacks_npz(
